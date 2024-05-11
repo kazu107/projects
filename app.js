6855baa4f7206c1118c3e516fc30d8f1ef2af4ef
kazu107 <kazu.uttu107@gmail.com>
2024-05-11 17:20:27 +0900

    tes

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ async function processInputs(inputs) {
 }
 
 
-const port = 3000;
+const port = 8022;
 server.listen(port, () => {
     console.log('Server running on http://localhost:' + port);
 });
