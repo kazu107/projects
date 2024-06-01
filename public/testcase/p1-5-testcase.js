49cf00a1dac4f95bda2f72068880925c1bcaebb7
kazuu <kazu.uttu107@gmail.com>
2024-06-01 09:03:12 +0900

    全ユーザーの解答追加

---
+++ public/testcase/p1-5-testcase.js
@@ -0,0 +1,27 @@
+module.exports = {
+    judge: "normal",
+    sample1: {
+        input: '2 1',
+        output: '5\n',
+    },
+    sample2: {
+        input: '-2 3',
+        output: '181\n',
+    },
+    test1: {
+        input: '10 10',
+        output: '9145669\n',
+    },
+    test2: {
+        input: '-10 -10',
+        output: '577266529\n',
+    },
+    test3: {
+        input: '-10 10',
+        output: '9166649\n',
+    },
+    test4: {
+        input: '10 -10',
+        output: '577245549\n',
+    },
+}
